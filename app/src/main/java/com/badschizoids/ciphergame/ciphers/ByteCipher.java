08ad49eb36bf752c7b1a84d3ec8233c932fc82fb
nekotxs <nekochan.txs@gmail,com>
2021-02-28 14:32:47 +0700

    AltByteCipher is ByteCipher now, old ByteCipher was deleted

---
+++ app/src/main/java/com/badschizoids/ciphergame/ciphers/ByteCipher.java
@@ -0,0 +1,39 @@
+package com.badschizoids.ciphergame.ciphers;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class ByteCipher implements EncryptAndDecrypt {
+
+    private char ch;
+
+    public ByteCipher() {
+        this.ch = 'e';
+    }
+
+    public ByteCipher(char ch) {
+        this.ch = ch;
+    }
+
+    @NotNull
+    private String xor(@NotNull String string) {
+        char[] chars = string.toCharArray();
+        for(int i = 0; i != string.length(); ++i) {
+            chars[i] = (char)(chars[i] ^ ch);
+        }
+        return new String(chars);
+    }
+
+    @NotNull
+    @Override
+    public String encrypt(@NotNull String string) {
+        return xor(string);
+    }
+
+    @NotNull
+    @Override
+    public String decrypt(@NotNull String string) {
+        return xor(string);
+    }
+}
