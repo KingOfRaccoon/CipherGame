f06e1c289c2b8ac68b8778098bc49bc3e20f2f45
nekotxs <nekochan.txs.work@gmail.com>
2021-03-07 13:05:46 +0700

    Неадекватный коммит

---
+++ app/src/main/java/com/badschizoids/ciphergame/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.badschizoids.ciphergame;
+
+import android.os.Bundle
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
